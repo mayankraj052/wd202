ad87782fea0aa899ba5f1c4bc8cc5ed00cbdbb3d
Mayank Raj <mayankraj052@gmail.com>
2024-09-09 23:23:23 +0530

    init

---
+++ http-server/eslint.config.mjs
@@ -0,0 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
+
+//npm install eslint --save-dev
+//npx eslint --init
