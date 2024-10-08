5b215b76d36e4217611e97f7eda6d6eff8e1c9db
Mayank Raj <mayankraj052@gmail.com>
2024-10-08 13:02:31 +0530

    Add pre-commit hook to run tests

---
+++ todo-cli/todo.js
@@ -0,0 +1,44 @@
+const todoList = () => {
+  all = [];
+
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todo) => todo.dueDate < today);
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => todo.dueDate === today);
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => todo.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((todo) => {
+        const checkbox = todo.completed ? "[x]" : "[ ]";
+        const displayDate = todo.dueDate === today ? "" : todo.dueDate;
+        return `${checkbox} ${todo.title} ${displayDate}`.trim();
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = { todoList };
