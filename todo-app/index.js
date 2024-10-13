70d412bcda1ecc78497d7f9dc98b91376e76fe81
Mayank Raj <mayankraj052@gmail.com>
2024-10-13 18:01:57 +0530

    solvinf error

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,6 @@
-const app = require("./app");
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
